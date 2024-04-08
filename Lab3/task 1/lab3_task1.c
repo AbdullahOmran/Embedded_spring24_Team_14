795f7ef8d9de9ae48d11422315532bed565290c4
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-04-08 19:03:30 +0200

    lab 3 is submitted

---
+++ Lab3/task 1/lab3_task1.c
@@ -0,0 +1,29 @@
+
+#define GPIOB_BASE_ADDR		0x40020400
+#define GPIOB_MODER			(*(unsigned long *)(GPIOB_BASE_ADDR + 0x00))
+#define GPIOB_OTYPER		(*(unsigned long *)(GPIOB_BASE_ADDR + 0x04))
+#define GPIOB_ODR			(*(unsigned long *)(GPIOB_BASE_ADDR + 0x14))
+#define RCC_AHB1ENR			(*(unsigned long *)(0x40023800 + 0x30))
+#define TRUE				1
+#define FALSE				0
+
+
+int main(void){
+
+	RCC_AHB1ENR |= (0x01 << 1);
+	GPIOB_MODER &= ~(0x03 << 3*2);
+	GPIOB_MODER |= (0x01 << 3*2);
+	GPIOB_OTYPER &= ~(0x01 << 3*1);
+	GPIOB_ODR &= ~(0x01 << 3*1);
+	GPIOB_ODR |= (0x01 << 3*1);
+	while(TRUE){
+		GPIOB_ODR &= ~(0x01 << 3*1);
+		unsigned char i = 0;
+		for(i = 0; i < 10000; i++){
+			__asm("nop");
+		}
+       GPIOB_ODR |= (0x01 << 3*1);
+	}
+
+	return 0;
+}
---
+++ Lab3/task 2/lab3_task2.c
@@ -0,0 +1,81 @@
+// Task 2///////////////////////////////////////////////////////////////////////////////////////////////////////
+#define GPIOB_BASE_ADDR		0x40020400
+#define GPIOB_MODER			(*(unsigned long *)(GPIOB_BASE_ADDR + 0x00))
+#define GPIOB_OTYPER		(*(unsigned long *)(GPIOB_BASE_ADDR + 0x04))
+#define GPIOB_PUPDR			(*(unsigned long *)(GPIOB_BASE_ADDR + 0x0c))
+#define GPIOB_ODR			(*(unsigned long *)(GPIOB_BASE_ADDR + 0x14))
+#define GPIOB_IDR			(*(unsigned long *)(GPIOB_BASE_ADDR + 0x10))
+#define RCC_AHB1ENR			(*(unsigned long *)(0x40023800 + 0x30))
+#define TRUE				1
+#define FALSE				0
+
+void initialize_LEDs(void);
+void initialize_button(void);
+void initialize(void);
+void delay(unsigned int milliseconds);
+
+int main(void){
+
+	initialize();
+	unsigned char btnState = 0;
+	unsigned char prevBtnState = 0;
+	unsigned char active_pin = 0;
+
+	while (TRUE)
+	{
+		// read button
+		btnState = (GPIOB_IDR & (0x01 << 3)) >> 3;
+		// detect falling edge (button press)
+		if(prevBtnState && !btnState){
+			// code for changing color of the LED
+			active_pin %= 8 ;
+			if (active_pin & (0x01)){
+				GPIOB_ODR &= ~(0x01 << 5);
+				GPIOB_ODR |= (0x01 << 5);
+			}
+			if ((active_pin & (0x01 << 1)) >> 1){
+				GPIOB_ODR &= ~(0x01 << 6);
+				GPIOB_ODR |= (0x01 << 6);
+			}
+			if ((active_pin & (0x01 << 2)) >> 2){
+				GPIOB_ODR &= ~(0x01 << 7);
+				GPIOB_ODR |= (0x01 << 7);
+			}
+
+			active_pin++;
+		}
+		// update prev state
+		prevBtnState = btnState;
+		// add delay for debouncing
+		delay(1000);
+	}
+	
+	return 0;
+}
+
+void initialize_LEDs(void){
+	for (unsigned int i = 5; i < 8; i++){
+		GPIOB_MODER &= ~(0x03 << i*2);
+		GPIOB_MODER |= (0x01 << i*2);
+		GPIOB_OTYPER &= ~(0x01 << i*1);
+		GPIOB_PUPDR &= ~(0x03 << i*2);
+	}
+}
+
+void initialize_button(void){
+	GPIOB_MODER &= ~(0x03 << 3*2);
+	GPIOB_OTYPER &= ~(0x01 << 3*1);
+	GPIOB_PUPDR &= ~(0x03 << 3*2);
+	GPIOB_PUPDR |= (0x01 << 3*2);
+}
+void initialize(void){
+	RCC_AHB1ENR |= (0x01 << 1);
+	initialize_LEDs();
+	initialize_button();
+}
+void delay(unsigned int milliseconds){
+	for (unsigned int i = 0; i < milliseconds; i++){
+        __asm("nop");
+    }
+}
+
